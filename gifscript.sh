2e3dfd1142ae92486d424c5a776c9e761f73bb45
aoliver3 <aoliver3@ggc.edu>
2015-11-04 05:14:57 +0000

    added scrolling start screen

--- gifscript.sh
+++ gifscript.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 clear
 user=`(whoami)`
+echo 
+echo
+echo "  ____ _  __ ____            _       _         _"
+sleep 0.3
+echo " / ___(_)/ _/ ___|  ___ _ __(_)_ __ | |_   ___| |__"  
+sleep 0.3
+echo "| |  _| | |_\___ \ / __| '__| | '_ \| __| / __| '_ \ " 
+sleep 0.3
+echo "| |_| | |  _|___) | (__| |  | | |_) | |_ _\__ \ | | | "
+sleep 0.3
+echo " \____|_|_| |____/ \___|_|  |_| .__/ \__(_)___/_| |_| "
+sleep 0.3
+echo "                              |_| "                    
+sleep 0.3
+echo
+sleep 0.3
+echo
+sleep 0.3
+echo "Created by..."
+sleep 0.3
+echo
+sleep 0.3
+echo
+sleep 0.3
+echo "       d8888        888       .d8888b.         888888 "                
+sleep 0.3
+echo "      d88888        888      d88P  '88b          '88b "                
+sleep 0.3
+echo "     d88P888        888      Y88b. d88P           888 "                
+sleep 0.3
+echo "    d88P 88888888b. 888888    'Y8888P'            888 .d88b.  .d88b. " 
+sleep 0.3
+echo "   d88P  888888 '88b888      .d88P88K.d88P        888d88''88bd8P  Y8b "
+sleep 0.3
+echo "  d88P   888888  888888      888'  Y888P'         888888  88888888888 "
+sleep 0.3
+echo " d8888888888888  888Y88b.    Y88b .d8888b         88PY88..88PY8b.     "
+sleep 0.3
+echo "d88P     888888  888 'Y888    'Y8888P' Y88b       888 'Y88P'  'Y8888 " 
+sleep 0.3
+echo "                                                .d88P "                
+sleep 0.3
+echo "                                              .d88P'  "                
+sleep 0.3
+echo "                                             888P'    "               
+sleep 1
+echo
 echo
 echo "Hello" $user
 echo
